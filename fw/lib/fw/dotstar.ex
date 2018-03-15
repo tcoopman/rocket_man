313fe0307561810736276103f044088222dfa1d1
Thomas Coopman <thomas.coopman@gmail.com>
2018-03-15 12:52:46 +0100

    some more tests. Maybe I should have a program to debug

--- fw/lib/fw/dotstar.ex
+++ fw/lib/fw/dotstar.ex
@@ -26,6 +26,13 @@ defmodule Fw.DotStar do
         GenServer.call(:dotstar, {:custom, nb, binary})
     end
 
+    def cycle_start(nb) do
+        GenServer.call(:dotstar, {:cycle_start, nb})
+    end
+    def move(speed, command) do
+        GenServer.call(:dotstar, {:move, 120, speed, command})
+    end
+
     def handle_call({:red,nb}, _from, pid) do
         red_command = for _ <- 1..nb, into: <<>>, do: <<255, 0, 0, 255>>
         full_command = start_frame() <> red_command <> end_frame()
@@ -48,20 +55,58 @@ defmodule Fw.DotStar do
         send_command(command,pid)
         {:reply, :ok, pid}
     end
+    def handle_call({:cycle_start, nb}, _from, pid) do
+        cycle(nb)
+        {:reply, :ok, pid}
+    end
+    def handle_call({:move, nb, speed, command}, _from, pid) do
+        move_(nb, speed, command)
+        {:reply, :ok, pid}
+    end
+    def handle_info({:cycle, nb}, pid) do
+        command = for _ <- 1..10, into: <<>>, do: <<231, nb*10, nb*10, nb*10>>
+        full_command = start_frame() <> command <> end_frame()
+
+        send_command(full_command, pid)
+        cycle(nb)
+        {:noreply, pid}
+    end
+    def handle_info({:moving, nb,speed, command}, pid) do
+        empty = for _ <- 1..(120 - nb), into: <<>>, do: <<224, 0, 0, 0>>
+        full_command = start_frame() <> empty <> command
+        send_command(full_command, pid)
+        move_(nb,speed, command)
+
+        {:noreply, pid}
+    end
 
     defp start_frame() do
         <<0,0,0,0>>
     end
 
     defp end_frame() do
-        <<255,255,255,255,255,255,255,255>>
+        <<255,255,255,255,255,255,255,255,255>>
     end
 
     defp send_command(command, pid) do
         command
+        # SPI.transfer(pid, command)
         |> :binary.bin_to_list
-        |> Enum.each(fn byte ->
-            _ = SPI.transfer(pid, :binary.encode_unsigned(byte))
+        |> Enum.chunk_every(20)
+        |> Enum.each(fn bytes ->
+            _ = SPI.transfer(pid, :binary.list_to_bin(bytes))
         end)
+    end
+
+    defp cycle(nb) do
+        if nb > 0 do
+            Process.send_after(self(), {:cycle, nb-1}, 200)
+        end
+    end
+
+    def move_(nb, speed, command) do
+        if nb > 0 do
+            Process.send_after(self(), {:moving, nb-1, speed, command}, speed)
+        end
     end
 end
